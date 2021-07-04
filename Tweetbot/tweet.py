3626fad55f1ea4a9b9627f748c14e7f3855c06b8
Nizar <40895148+nizariyf@users.noreply.github.com>
2021-07-04 16:56:06 +0700

    v1.0.0

--- Tweetbot/tweet.py
+++ Tweetbot/tweet.py
@@ -58,10 +58,6 @@ options = Options()
 options.headless = True
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
-options.add_argument('--window-size=1920,1080')
-options.add_argument('--start-maximized')
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--allow-running-insecure-content')
 
 with open('.config.json') as json_data:
   cfg = json.load(json_data)
