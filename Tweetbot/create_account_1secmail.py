d6259e2f32e456128facd8a34ccaf76a65477be3
Nizar <40895148+nizariyf@users.noreply.github.com>
2021-07-04 16:54:28 +0700

    Update create_account_1secmail.py

--- Tweetbot/create_account_1secmail.py
+++ Tweetbot/create_account_1secmail.py
@@ -57,10 +57,6 @@ options = Options()
 options.headless = True
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
-options.add_argument('--window-size=1920,1080')
-options.add_argument('--start-maximized')
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--allow-running-insecure-content')
 
 class Create:
   def __init__(self, name, email, password):
